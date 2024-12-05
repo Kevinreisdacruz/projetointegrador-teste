13f014a3e13bbd1efb1264d475dbaed96dee9c99
KEVIN REIS DA CRUZ <kevin.rcruz@senacsp.edu.br>
2024-12-05 17:07:21 -0300

    arrumando tela de agradecimento

--- agradecimento.php
+++ agradecimento.php
@@ -1,23 +1,15 @@
 <?php
-  include'navbar.php'
+include 'navbar.php'
 ?>
 
-  <div class="container-obg">
-    <div class="box-interior-obg">
-
-      <div class="obg">
-        <h1 style=" font-family: Arial, Helvetica, sans-serif;">OBRIGADO POR COMPRAR CONOSCO<br><br> REALE GELATO AGRADECE SUA PREFERENCIA<br><br>SEU PEDIDO ESTÁ SENDO
-          PREPARADO</h1>
-      </div>   
-      <img src="imagelogos/reale-gelato.png" alt="" width="300px">
-
-      <a style="text-decoration: none;" href="index.html">
-        <button class="btn-obg">VOLTAR</button>
-      </a>kevinreis
+<div class="container-agradecimento">
+  <div class="fundo-agradecimento">
+    <div class="escrita">
 
     </div>
   </div>
+</div>
 
 <?php
-  include'footer.php'
+include 'footer.php'
 ?>
