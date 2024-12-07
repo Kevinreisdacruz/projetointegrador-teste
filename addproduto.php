e42af7c8a0547ce7b03bf7b70e7840c11c52f051
KevinReis <kevinreisdacruz10@gmailcom>
2024-12-07 20:04:03 -0300

    feito a  a pagina administracao, tambem foi feito as paginas de adicionar catalogo e adicioanr produto

---
+++ addproduto.php
@@ -0,0 +1,35 @@
+<?php
+include 'navbar.php'
+?>
+
+
+<div id="container-add-produto">
+    <div class="fundo-add-produto">
+
+        <div class="titulo-add-produto">
+            <h2 style="font-weight: bold;">ADICIONAR PRODUTO</h2>
+        </div>
+
+        <div class="inf-add-produto">
+            <input type="text" required='required' placeholder="NOME DO PRODUTO">
+            <input type="text" required='required' placeholder="DESCRIÇÃO">
+            <input type="text" id="fone" required='required' placeholder="PREÇO">
+            <input type="file">
+        </div>
+
+        <div class="botoes-add-produto">
+            <a href="index.php">
+                <button class="btn-add-produto">ADICIONAR PRODUTO</button><br>
+            </a>
+            <a href="administracao.php">
+                <button class="btn-add-produto">CANCELAR</button>
+            </a>
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+include 'footer.php'
+?>
---
+++ administracao.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'navbar.php';
+
+?>
+
+<div class="container-admin">
+    <div class="opcoes-admin">
+
+        <div class="escrita-admin">
+            <h3>O QUE DESEJA FAZER?</h3>
+        </div>
+        <div class="img-admin">
+            <a href="addproduto.php">
+                <img class="icons-admin" src="imagelogos/adicionar-produto.png" title="ADICIONAR PRODUTO" alt="" width="100px">
+            </a>
+            <a href="">
+                <img class="icons-admin" src="imagelogos/catalogo-on-line.png" title="ADICIONAR CATÁLOGO" alt="" width="65px">
+            </a>
+            <img class="icons-admin" src="imagelogos/cliente.png" alt="" title="CLIENTES" width="65px">
+            <img class="icons-admin" src="imagelogos/configs-admin.png" title="ADMINS" alt="" width="65px">
+        </div>
+
+
+    </div>
+</div>
+
+<?php
+include 'footer.php'
+?>
